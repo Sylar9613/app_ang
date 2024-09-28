e50cb5e282cee4d524e3517dcbfce1248192758f
Sylar9613 <ariancastellanosrodriguez@gmail.com>
2024-09-28 20:21:42 +0200

    Modificando los parametros de conexion a firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyB4bNxzZrYS6nINdk8FXvKS6YZgcrNU2aY",
+        authDomain: "mi-primera-app-en-fireba-afe84.firebaseapp.com",
+        databaseURL: "https://mi-primera-app-en-fireba-afe84-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "mi-primera-app-en-fireba-afe84",
+        storageBucket: "mi-primera-app-en-fireba-afe84.appspot.com",
+        messagingSenderId: "180594534844",
+        appId: "1:180594534844:web:83d1856e6529b5de0ca3b9",
+        measurementId: "G-21RCED78V1"
+    }
+};
