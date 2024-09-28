88f9dae29a0ac5509411a63c52793723a0a99be1
Sylar9613 <ariancastellanosrodriguez@gmail.com>
2024-09-28 19:49:05 +0200

    Agregando el fichero de config de tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['selector', '[data-mode="dark"]'],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  /* darkMode: ['selector', '[data-mode="dark"]'],
+  content: [
+    "./src/aqui van dos ** /*.{html,ts}",
+  ], */
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
