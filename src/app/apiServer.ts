b184f2e9171510b067f64f2e3d9304e20e88acaa
Sylar9613 <ariancastellanosrodriguez@gmail.com>
2024-09-28 19:59:43 +0200

    Creando las variables para la conexion al apiServer

---
+++ src/app/apiServer.ts
@@ -0,0 +1,4 @@
+export const apiServer = {
+    serverUrl: 'https://mi-primera-app-en-fireba-afe84-default-rtdb.europe-west1.firebasedatabase.app/datos.json',  // Replace with your API server URL
+    siteName: 'Empleados', // Replace with your
+}
