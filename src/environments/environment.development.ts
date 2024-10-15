94dc4b7fd0b3dd9fafcbc0ffeae1176c303b3e87
Sylar9613 <ariancastellanosrodriguez@gmail.com>
2024-10-15 20:41:13 +0200

    Agregando las variables de entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+export const environment = {
+    firebase: {
+        "projectId": "mi-primera-app-en-fireba-afe84",
+        "appId": "1:180594534844:web:83d1856e6529b5de0ca3b9",
+        "databaseURL": "https://mi-primera-app-en-fireba-afe84-default-rtdb.europe-west1.firebasedatabase.app",
+        "storageBucket": "mi-primera-app-en-fireba-afe84.appspot.com",
+        "apiKey": "AIzaSyB4bNxzZrYS6nINdk8FXvKS6YZgcrNU2aY",
+        "authDomain": "mi-primera-app-en-fireba-afe84.firebaseapp.com",
+        "messagingSenderId": "180594534844",
+        "measurementId": "G-21RCED78V1"
+    },
+    production: false
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
     firebase: {
-        apiKey: "AIzaSyB4bNxzZrYS6nINdk8FXvKS6YZgcrNU2aY",
-        authDomain: "mi-primera-app-en-fireba-afe84.firebaseapp.com",
-        databaseURL: "https://mi-primera-app-en-fireba-afe84-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "mi-primera-app-en-fireba-afe84",
-        storageBucket: "mi-primera-app-en-fireba-afe84.appspot.com",
-        messagingSenderId: "180594534844",
-        appId: "1:180594534844:web:83d1856e6529b5de0ca3b9",
-        measurementId: "G-21RCED78V1"
-    }
+        "projectId": "mi-primera-app-en-fireba-afe84",
+        "appId": "1:180594534844:web:83d1856e6529b5de0ca3b9",
+        "databaseURL": "https://mi-primera-app-en-fireba-afe84-default-rtdb.europe-west1.firebasedatabase.app",
+        "storageBucket": "mi-primera-app-en-fireba-afe84.appspot.com",
+        "apiKey": "AIzaSyB4bNxzZrYS6nINdk8FXvKS6YZgcrNU2aY",
+        "authDomain": "mi-primera-app-en-fireba-afe84.firebaseapp.com",
+        "messagingSenderId": "180594534844",
+        "measurementId": "G-21RCED78V1"
+    },
+    production: true
 };
