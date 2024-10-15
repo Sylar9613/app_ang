18d49cbef42a9846af9373ed05aba96d3c0af7c2
Sylar9613 <ariancastellanosrodriguez@gmail.com>
2024-10-15 20:45:14 +0200

    Agregando el MatSpinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { AuxiliarComponent } from "./auxiliar/auxiliar.component";
+import { EmpleadosComponent } from "./empleados/empleados.component";
+import { HeaderComponent } from "./header/header.component";
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ConfirmInterceptor } from './confirm-interceptor.interceptor';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HttpClientModule, RouterOutlet, CalculadoraComponent, AuxiliarComponent, EmpleadosComponent, HeaderComponent, MatProgressSpinnerModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ConfirmInterceptor, multi: true }
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngApp';
+  
 }
