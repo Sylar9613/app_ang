2a1de0e93c6d8af8490fa5189cc405a5df77fd76
Sylar9613 <ariancastellanosrodriguez@gmail.com>
2024-10-15 20:44:32 +0200

    Agregando las rutas y canActivate()

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { AuxiliarComponent } from './auxiliar/auxiliar.component';
+import { HomeComponent } from './home/home.component';
+import { EmpleadosListComponent } from './empleados/empleados-list/empleados-list.component';
+import { Error404Component } from './error404/error404.component';
+import { LoginComponent } from './login/login.component';
+import { LoginGuard } from './login/login-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+    // Add more routes here...
+    { path: '', component: HomeComponent },
+    { path: 'calculadora', component: CalculadoraComponent },
+    { path: 'empleados', component: EmpleadosComponent, canActivate: [LoginGuard] },
+    { path: 'auxiliar', component: AuxiliarComponent },
+    { path: 'nuevo', component: EmpleadosListComponent },
+    { path: 'actualizar/:id', component: EmpleadosListComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', component: Error404Component },
+];
